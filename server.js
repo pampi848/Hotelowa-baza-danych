48aca3ad5b26f252d71a842f68ee5ef519cc65e4
pampi <pampi.com@gmail.com>
2016-09-20 22:43:28 +0200

    Postawienie projektu na nogi.

--- server.js
+++ server.js
@@ -1,41 +1,62 @@
 var express = require('express');
 var path = require('path');
-var app = express();
-var pageName = 'index';
-const response404 = 'response404';
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
-app.use(express.static(path.join(__dirname, 'public/static/')));
+var app = express();
+
+app.disable('x-powered-by');
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', function (req, res) {
-    var actionGet = req.query.action; // $_GET["id"]
-    switch(actionGet){
-        case ('index' || 'undefined' || '' || null):
-            pageName = 'index';
-            break;
-        case "wynajem":
-            pageName = 'wynajem';
-            break;
-        default:
-            pageName = response404;
-            break;
+app.use(express.static(path.join(__dirname, '/public')));
 
-    }
-    typeof(actionGet)==('undefined')||actionGet==''?pageName='index':'';
-    console.log(actionGet);
-        res.sendFile('public/' + pageName + '.html', {root: __dirname}, function (e){
-            if(typeof(e)=='object' && e.status==404){
-                console.log(e);
-                console.log('Wysłano odpowiedź 404');
-                res.sendFile('public/' + response404 + '.html', {root: __dirname});
-            }
-            else{
-                console.log('Wysłano plik public/' + pageName + '.html');
-            }
-        });
+app.use(function (req, res, next) {
+    console.log("Looking for URL : " + req.url);
+    next();
 });
- 
-app.listen(app.get('port'), function(){
-    console.log("Serwer is working at 127.0.0.1:3000");
+
+app.get('/', function (req, res){
+    res.render('home');
+});
+
+app.get('/about', function (req, res){
+    res.render('about');
+});
+
+app.get('/rooms', function (req, res){
+    res.render('roomList');
+});
+
+//
+app.get('/junk', function () {
+    console.log('Tried to access /junk');
+    throw new Error('/junk doesn\'t exist');
+});
+
+app.use(function (err, req, res, next) {
+    console.log('Error : ' + err.message);
+    next();
+});
+//
+
+app.use(function (req, res) {
+    res.type('text/html');
+    res.status(404);
+    res.render('404');
+    console.log('Response : 404');
+});
+
+app.use(function (err, req, res) {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+
+app.listen(app.get('port'), function () {
+    console.log('Express started at 127.0.0.1:' + app.get('port'));
 });
