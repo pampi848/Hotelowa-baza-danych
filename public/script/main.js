4b8aa6892baf2fd9a567f0c0b7d1edeaf3eb377c
pampi <pampi.com@gmail.com>
2016-09-27 23:22:47 +0200

    Including favicon / logo.

--- public/script/main.js
+++ public/script/main.js
@@ -5,15 +5,15 @@
 $(function() {
     $('#logo')
         .mouseover(function() {
-            var src = "/img/logo-inverse.png";
+            var src = "/img/logo-hover.png";
             $(this).attr("src", src);
         })
         .mouseout(function() {
-            var src = "/img/logo-gray.png";
+            var src = "/img/logo.png";
             $(this).attr("src", src);
         })
         .click(function() {
-            var src = "/img/logo-yellow.png";
+            var src = "/img/logo-clicked.png";
             $(this).attr("src", src);
         });
 });
